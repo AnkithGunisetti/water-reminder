340b6dc8dbec9ede33d24ab3079626bedeb4dd70
Ankith Gunisetti <ankithkumar0301@gmail.com>
2026-02-06 22:59:26 +0530

    Updated scheduler with IST timezone

--- water-remainder/src/main/java/com/example/demo/service/EmailService.java
+++ water-remainder/src/main/java/com/example/demo/service/EmailService.java
@@ -1,27 +1,62 @@
 package com.example.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    @Value("${BREVO_API_KEY}")
+    private String apiKey;
 
     public void sendReminderEmail(String toEmail) {
 
-    	 SimpleMailMessage message = new SimpleMailMessage();
+        try {
 
-         message.setFrom("ankithgunisetti@gmail.com");  // VERIFIED sender
-         message.setTo(toEmail);
-         message.setSubject("Water Reminder 💧");
-         message.setText("Time to drink water!");
+            URL url = new URL("https://api.brevo.com/v3/smtp/email");
 
-         mailSender.send(message);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-         System.out.println("Email sent to: " + toEmail);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("accept", "application/json");
+            conn.setRequestProperty("api-key", apiKey);
+            conn.setRequestProperty("content-type", "application/json");
+
+            conn.setDoOutput(true);
+
+            String json = "{"
+                    + "\"sender\":{\"email\":\"ankithkumar0301@gmail.com\"},"
+                    + "\"to\":[{\"email\":\"" + toEmail + "\"}],"
+                    + "\"subject\":\"Water Reminder 💧\","
+                    + "\"textContent\":\"Time to drink water! Stay hydrated.\""
+                    + "}";
+
+            OutputStream os = conn.getOutputStream();
+            os.write(json.getBytes());
+            os.flush();
+            os.close();
+
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == 201) {
+
+                System.out.println("Email sent successfully to: " + toEmail);
+
+            } else {
+
+                System.out.println("Failed to send email. Code: " + responseCode);
+
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
     }
 }
